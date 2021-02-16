8c58b98bc2fe11c482bfb5c17be0c2e9ccb36011
Yvon21 <78088727+Yvon21@users.noreply.github.com>
2021-02-16 10:16:39 +0100

    Create Cables.java

---
+++ Cables.java
@@ -0,0 +1,14 @@
+public class Cables extends EqReseaux{
+	String input;
+	String output;
+	
+	public Cables(){
+		super();
+	}
+		
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
