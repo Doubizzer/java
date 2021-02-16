01189c9d7be5486f8e4309b97a89484b961f4a6e
teobla <78786269+teobla@users.noreply.github.com>
2021-02-16 10:05:18 +0100

    Create EqClient.java

---
+++ EqClient.java
@@ -0,0 +1,18 @@
+
+public class EqClient extends EqReseau{
+	String aIP;
+	String aMAC;
+	String aGate;
+	
+	public EqClient() {
+		super ();
+	}
+	
+	public EqClient(String pIP, String pMAC, String pGate, String pNom) {
+		aIP = pIP;
+		aMAC = pMAC;
+		aGate = pGate;
+		this.type = "PC";
+		this.nom = pNom;
+	}
+}
