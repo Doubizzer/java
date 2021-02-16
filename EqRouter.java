1f4086b6ae9d3e77e0135fa7207c0e00591220f5
teobla <78786269+teobla@users.noreply.github.com>
2021-02-16 10:13:04 +0100

    Create EqRouter.java

---
+++ EqRouter.java
@@ -0,0 +1,11 @@
+
+public class EqRouter extends EqReseau {
+	
+	public EqRouter() {
+		this.nom = "Inconnu";
+		this.type = "Routeur";
+	}
+	public EqRouter(String p1, String p2) {
+		super(p1, p2);
+	}
+}
