981ced135a67410609d3fa8d97f65a881a32eca7
teobla <78786269+teobla@users.noreply.github.com>
2021-02-16 09:46:37 +0100

    Create EqReseau.java

---
+++ EqReseau.java
@@ -0,0 +1,15 @@
+
+public class EqReseau {
+	String nom;
+	String type;
+	
+	public EqReseau() {
+		nom = "Inconnu";
+		type = "Inconnu";
+	}
+	
+	public EqReseau(String pNom,String pType) {
+		nom = pNom;
+		type = pType;
+	}
+}
