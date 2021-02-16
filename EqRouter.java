3148d2eec7296f974dce01aed3ad19a728772424
Nathan Rihet <50133907+NathanKneT@users.noreply.github.com>
2021-02-16 10:37:32 +0100

    Update EqRouter.java

--- EqRouter.java
+++ EqRouter.java
@@ -8,4 +8,6 @@ public class EqRouter extends EqReseau {
 	public EqRouter(String p1, String p2) {
 		super(p1, p2);
 	}
+	public String toString() {
+   		return "Nom de la machine : " + this.nom + " Type de la machine : " + this.type;
 }
