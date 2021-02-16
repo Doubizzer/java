e86e477cc79b0b9fda6d2a39b33b1d7b60b5de33
Nathan Rihet <50133907+NathanKneT@users.noreply.github.com>
2021-02-16 10:18:28 +0100

    Update EqSwitch.java

--- EqSwitch.java
+++ EqSwitch.java
@@ -1,10 +1,10 @@
 public class EqSwitch extends EqReseau {
 	        array[];
-
+	
 	public EqSwitch() {
-		      this.array[] = null;
+		this.array[] = null;
 	}
 
 	public EqSwitch() {
-		      array
+		array
           }
