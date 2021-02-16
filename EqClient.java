20f7eddcf2b82a29319644118bc091305dd20619
Nathan Rihet <50133907+NathanKneT@users.noreply.github.com>
2021-02-16 10:40:05 +0100

    Update EqClient.java

--- EqClient.java
+++ EqClient.java
@@ -15,4 +15,6 @@ public class EqClient extends EqReseau{
 		this.type = "PC";
 		this.nom = pNom;
 	}
+	public String toString() {
+   		return "Nom de la machine : " + this.nom + " Type de la machine : " + this.type + "IP de la machine :" + this.aIP + "MAC de la machine" + this.aMAC + "Gateway :" + this.aGate;
 }
