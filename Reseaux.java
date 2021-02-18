5fc551e38f5764517c02a42553a57eca6ef66e31
Nathan Rihet <50133907+NathanKneT@users.noreply.github.com>
2021-02-18 19:53:35 +0100

    Update Reseaux.java

--- Reseaux.java
+++ Reseaux.java
@@ -1,1 +1,17 @@
+import java.util.ArrayList;
 public class Reseaux { 
+      Arraylist tablist = new Arraylist();   
+      public Reseaux() {
+        
+      }
+      public void addEqClient (EqClient pNom){
+          tablist.add("EqClient =>" + pNom);
+      }
+      public void addEqSwitch (EqSwitch pNom){
+          tablist.add("EqSwitch =>" + pNom);
+      }
+  
+      public void affiche() {
+        System.out.println(tablist);
+    }
+}
